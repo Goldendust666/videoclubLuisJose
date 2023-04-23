4ddc42279abc30abc045c828a5c999cd62f11a7a
Usuario <Usuario@DESKTOP-IP8O1BD>
2023-04-23 13:06:40 +0200

    V1.1.1 Realizadas las clases clientes y peliculas (se me habia olvidado hacerles commit)

--- src/videoclublj/Cliente.java
+++ src/videoclublj/Cliente.java
@@ -4,10 +4,59 @@
  */
 package videoclublj;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Usuario
  */
 public class Cliente {
-    
+
+    public int codigo;
+    public String nombre;
+    public String apellidos;
+    public ArrayList alquilada;
+
+    public Cliente() {
+    }
+
+    public Cliente(int codigo, String nombre, String apellidos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public boolean alquilada(Pelicula pelicula){
+        if(alquilada.contains(pelicula)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 }
+
--- src/videoclublj/pelicula.java
+++ src/videoclublj/pelicula.java
@@ -8,6 +8,48 @@ package videoclublj;
  *
  * @author Usuario
  */
-public class pelicula {
+public class Pelicula {
+    public int codigo;
+    public String titulo;
+    public String director;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(int codigo, String titulo, String director) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.director = director;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "codigo=" + codigo + ", titulo=" + titulo + ", director=" + director + '}';
+    }
     
+
 }
